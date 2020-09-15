8a757d7c5a5a2b5d26161a4be5b919696a14444f
piano <maryesmaeely@gmail.com>
2020-09-15 12:04:30 +0200

    employeeMenu updated. Only the 7th option works for now. Password: password123

--- src/DART/MainProgram.java
+++ src/DART/MainProgram.java
@@ -25,10 +25,6 @@ public class MainProgram {                                                      
                 "Go to Employee menu",
                 "Go to Customer menu",
                 "Exit system",
-                "HEllo",
-                "gfsdgsfh",
-                "gfsdgfsd",
-                "fdsfa"
         };
 
         String inputPrompt = "Enter choice: ";
@@ -37,7 +33,7 @@ public class MainProgram {                                                      
                                                                                 // This should be handled in "InputHandler".
         switch (choice) {                                                       // choice has become the length of "String[] mainMenuItems".
             case 1 -> managerMenu();                                            // If user inputs 1 then we go to managerMenu method
-            case 2 -> employeeMenu();
+            case 2 -> employeeMenu(input);
             case 3 -> customerMenu();
             case 4 -> exitMenu();
             default -> System.out.println("Please Choose a correct number: ");
@@ -51,18 +47,40 @@ public class MainProgram {                                                      
                 "2. View all employees\n" +
                 "3. Return to Main Menu\n");
     }
-    public static void employeeMenu () { // Employee menu
+    public static void employeeMenu (Scanner input) { // Employee menu
         DART.Utilities.DartUtilities.clearConsole();
-        System.out.print("Employee Screen - Type one of the options below:\n" +
-                "1. Register a game\n" +
-                "2. Remove a game\n" +
-                "3. Register a customer\n" +
-                "4. Remove a customer\n" +
-                "5. Show total rent profit\n" +
-                "6. View all games\n" +
-                "7. Return to Main Menu\n" +
-                "Enter choice: ");
+
+        String password = "";
+        System.out.println("Insert password:");
+        password = input.nextLine();
+
+        if (password.equals("password123")) {
+            String prompt = "Employee Screen - Type one of the options below:";
+
+            String[] employeeMenuItems = {
+                    "Register a game",
+                    "Remove a game",
+                    "Register a customer",
+                    "Remove a customer",
+                    "Show total rent profit",
+                    "View all games",
+                    "Return to Main Menu",
+            };
+            String inputPrompt = "";
+
+            int choice = MenuHandler.chooseFromMenu(input, employeeMenuItems, prompt, inputPrompt);  // Goes into the MenuHandler class. MenuHandler prints the "prompt" and "mainMenuItems"
+            // This should be handled in "InputHandler".
+            switch (choice) {                                                       // choice has become the length of "String[] mainMenuItems".
+                case 7 -> mainMenu(input);                                            // If user inputs 1 then we go to managerMenu method
+                default -> System.out.println("Please Choose a correct number: ");
+            }
+        } else {
+            System.out.println("INVALID PASSWORD");
+            mainMenu(input);
+        }
     }
+
+
     public static void customerMenu () { // Customer menu
         DART.Utilities.DartUtilities.clearConsole();
         System.out.print("Customer Screen - Type one of the options below:\n" +
