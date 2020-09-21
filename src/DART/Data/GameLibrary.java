2a9e68632c10585a7bfef667f54b5d544f6c2326
lucasnordic <65816176+lucasnordic@users.noreply.github.com>
2020-09-21 22:15:07 +0200

    Update GameLibrary.java

--- src/DART/Data/GameLibrary.java
+++ src/DART/Data/GameLibrary.java
@@ -1,4 +1,38 @@
 package DART.Data;
 
 public class GameLibrary {
+    private Game[] games; //array for games
+    private int nextPosition;
+
+    public GameLibrary() {
+        this.games = new Game[10];
+
+    }
+
+    public void addGame(Game game) {
+        if (nextPosition < this.games.length) {
+            games[nextPosition] = game;
+            nextPosition++;
+        } else {
+            Game[] biggerGame = new Game[games.length + 1];
+            for (int i = 0; i < games.length; i++) {
+                biggerGame[i] = games[i];
+            }
+            this.games = biggerGame;
+            biggerGame[nextPosition] = game;
+            nextPosition++;
+
+        }
+    }
+
+    public void removeAGame(int id) {
+        for (int i = 0; i < games.length; i++) {
+            if (games[i] != null && games[i].getId() == id) {
+                games[i] = null;
+                System.out.println("Game is removed!");
+                return;
+            }
+        }
+        System.out.println("Game with id #" + id + " not found");
+    }
 }
