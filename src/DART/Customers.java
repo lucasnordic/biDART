1069c7f75a2079c834d53a4bcdd5dbfcd3b3a68a
piano <maryesmaeely@gmail.com>
2020-09-22 22:40:54 +0200

    Customer codes added

---
+++ src/DART/Customers.java
@@ -0,0 +1,27 @@
+package DART;
+
+import java.util.ArrayList;
+
+public class Customers {
+
+
+    public static void registration() {
+
+
+        System.out.println("Creating a Customer. Please type the customer’s:\n" + "ID:  ");
+
+        int min = 0;
+        int max = 10000000;
+        int ID = ScannerInput.inputInt (min, max);
+
+        System.out.print("\nName:   ");
+        String name = ScannerInput.inputString();
+
+        Customer person = new Customer(ID, name);
+
+        ArrayList<Customer> allCustomers = new ArrayList <Customer>();
+        allCustomers.add(person);
+
+        Menu.employeeMenu();
+    }
+}
