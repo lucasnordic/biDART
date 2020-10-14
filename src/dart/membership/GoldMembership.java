4a1b4800915437905d46da2258a1245d21c26954
Lucas <lucas.nordgren@live.se>
2020-10-15 00:14:28 +0200

    Small changes

--- src/dart/membership/GoldMembership.java
+++ src/dart/membership/GoldMembership.java
@@ -2,37 +2,47 @@ package dart.membership;
 
 public class GoldMembership implements Membership {
 
+
+    /**
+     * Attributes:
+     * (Question, is it better to declare them like this?
+     * I think it is better to keep the numbers upp here since,
+     * if this class were to become very long then we can still change
+     * these numbers easily.
+     */
+
+    private final double discount = 0.15;
+    private final int maxAllowedRent = 5;
+    private final int additionalCredit = 2;
+    private final String membershipClass = "Gold";
+
+
     /**
      * Overridden methods:
      */
 
     @Override
     public double getDiscount() {
-        return 0.15;
+        return this.discount;
     }
 
     @Override
     public int getMaxAllowedRent() {
-        return 5;
+        return this.maxAllowedRent;
     }
 
     @Override
     public int getAdditionalCredit() {
-        return 2;
+        return this.additionalCredit;
     }
 
     @Override
     public String getMembershipClass() {
-        return "Gold";
+        return this.membershipClass;
     }
 
     @Override
     public double calculatePrice (double price) {
         return (1 - getDiscount()) * price;
     }
-
-//    @Override
-//    public String toString() {
-//        return super.toString();
-//    }
 }
--- src/dart/membership/PlatinumMembership.java
+++ src/dart/membership/PlatinumMembership.java
@@ -2,28 +2,43 @@ package dart.membership;
 
 public class PlatinumMembership implements Membership{
 
+
+    /**
+     * Attributes:
+     * (Question, is it better to declare them like this?
+     * I think it is better to keep the numbers upp here since,
+     * if this class were to become very long then we can still change
+     * these numbers easily.
+     */
+
+    private final double discount = 0.25;
+    private final int maxAllowedRent = 7;
+    private final int additionalCredit = 3;
+    private final String membershipClass = "Platinum";
+
+
     /**
      * Overridden methods:
      */
 
     @Override
     public double getDiscount() {
-        return 0.25;
+        return this.discount;
     }
 
     @Override
     public int getMaxAllowedRent() {
-        return 7;
+        return this.maxAllowedRent;
     }
 
     @Override
     public int getAdditionalCredit() {
-        return 3;
+        return this.additionalCredit;
     }
 
     @Override
     public String getMembershipClass() {
-        return "Platinum";
+        return this.membershipClass;
     }
 
     @Override
