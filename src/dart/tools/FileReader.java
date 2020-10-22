d969d1bdf9c6d7a8af2af7c88d06b0b8c26eacfc
Lucas <lucas.nordgren@live.se>
2020-10-22 14:15:04 +0200

    fixxed messaging system

--- src/dart/tools/FileReader.java
+++ src/dart/tools/FileReader.java
@@ -1,8 +1,7 @@
 package dart.tools;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
+import java.util.Scanner;
 
 public class FileReader extends Reader {
 
@@ -26,3 +25,68 @@ public class FileReader extends Reader {
 
     }
 }
+
+//package dart.storage;
+//
+//        import java.io.*;
+//        import java.util.Scanner;
+//
+//
+//public class Storage extends Reader {
+//    private static String filePath = "./files/storage.txt";
+//
+//
+//
+//    public void readStorage() {
+//        try {
+////            FileOutputStream fs = new FileOutputStream("foo.ser");
+////            ObjectInputStream os = new ObjectInputStream(fs);
+////            os.
+////            os.c
+//            File myFile = new File(filePath);
+//
+//            if (!myFile.exists()) {
+//                myFile.getParentFile().mkdirs();
+//                myFile.createNewFile();
+//                writeStorage();
+//            }
+//
+//            Scanner myScanner = new Scanner(myFile);
+//            while (myScanner.hasNextLine()) {
+//                System.out.println(myScanner.nextLine());
+//            }
+//        } catch (IOException e) {
+//            System.out.println("The file was not found");
+//            e.printStackTrace();
+//        }
+////        FileReader file = new FileReader();
+////        BufferedReader fileInput = new BufferedReader(file);
+////
+////        for (int i = 0; i < 3; i++) {
+////            System.out.println(fileInput.readLine());
+////        }
+////        fileInput.close();
+//    }
+//
+//    public static void writeStorage() {
+//        try {
+//            FileWriter myWriter = new FileWriter(filePath);
+//            myWriter.write("test");
+//            myWriter.close();
+//        } catch (IOException e) {
+//            System.out.println("Something wrong happened.");
+//            e.printStackTrace();
+//        }
+//
+//    }
+//
+//    @Override
+//    public int read(char[] cbuf, int off, int len) throws IOException {
+//        return 0;
+//    }
+//
+//    @Override
+//    public void close() throws IOException {
+//
+//    }
+//}
