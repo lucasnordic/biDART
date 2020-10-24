62dfebd85b5e9350ad606ee46373f66c54f88103
Lucas <lucas.nordgren@live.se>
2020-10-24 15:55:34 +0200

    Small changes to MessageController

--- src/dart/controller/MessageController.java
+++ src/dart/controller/MessageController.java
@@ -11,12 +11,14 @@ import java.util.ArrayList;
 public class MessageController {
     private ArrayList<Message> messageList = new ArrayList<>();
 
+    // Gets the list of messages to a user depending on what kind of user it is
     public ArrayList<Message> getMessageListForUser(User user) {
         ArrayList<Message> userMessages = new ArrayList<>();
 
         for (Message message : messageList) {
             if (message.isOfType("employee") && user instanceof Employee) {
                 userMessages.add(message);
+
             } else if (message.isOfType("personal") && user instanceof Customer) {
                 if (message.getMessageTo().equals(user.getId())) {
                     userMessages.add(message);
@@ -27,6 +29,7 @@ public class MessageController {
         return userMessages;
     }
 
+    // Adds a message to the messageList "database".
     public Message addMessageToList(String message, String name, String messageFromId, String messageToUserId, String type) {
         Message newMessage = new Message(message, name, messageFromId, messageToUserId, type);
         messageList.add(newMessage);
@@ -34,37 +37,6 @@ public class MessageController {
         System.out.println("Message sent! ");
 
         return newMessage;
-    }
-
-    public void removeMessage(int choice){
-        messageList.remove(choice);
-        System.out.println("The message you wanted to remove has been deleted.");
-//        for (Message message: messageList) {
-//            if (message.getMessageId().equals(choice)){
-//                messageList.remove(message);
-//                System.out.println("The message you wanted to remove has been deleted.");
-//                return;
-//            }
-//        }
-    }
-
-    // Removes a message from the message list based on the message id.
-    public void removeMessageFromList(String messageId) {
-//        Message messageFound = null;
-        int index = 0;
-
-        while(index < messageList.size()) {
-            Message message = messageList.get(index);
-
-            if (message.getMessageId().equals(messageId)) {
-//                messageFound = message
-                messageList.remove(index);
-                System.out.print("Message was removed! ");
-                UserInputHandler.pressAnyKeyCon();
-            } else {
-                index++;
-            }
-        }
     }
 
     // Removes a message from the message list based on a customers id.
@@ -86,3 +58,35 @@ public class MessageController {
         }
     }
 }
+
+
+//    public void removeMessage(int choice){
+//        messageList.remove(choice);
+//        System.out.println("The message you wanted to remove has been deleted.");
+//        for (Message message: messageList) {
+//            if (message.getMessageId().equals(choice)){
+//                messageList.remove(message);
+//                System.out.println("The message you wanted to remove has been deleted.");
+//                return;
+//            }
+//        }
+//    }
+//
+//    // Removes a message from the message list based on the message id.
+//    public void removeMessageFromList(String messageId) {
+////        Message messageFound = null;
+//        int index = 0;
+//
+//        while(index < messageList.size()) {
+//            Message message = messageList.get(index);
+//
+//            if (message.getMessageId().equals(messageId)) {
+////                messageFound = message
+//                messageList.remove(index);
+//                System.out.print("Message was removed! ");
+//                UserInputHandler.pressAnyKeyCon();
+//            } else {
+//                index++;
+//            }
+//        }
+//    }
