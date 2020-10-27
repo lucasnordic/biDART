1ce1de12a30a3b2cd2fff4fed99d002f01a4bd87
Lucas <lucas.nordgren@live.se>
2020-10-27 21:43:11 +0100

    final edits...

--- src/dart/Main.java
+++ src/dart/Main.java
@@ -9,19 +9,19 @@ import dart.view.DartView;
  * </www.biDART.com>
  *
  * <Changes from milestone 1 to milestone 2:
- *  "To implement "Epic feature 8", I modified the "class Dart/DartView" by <adding> : <method> : loginCheckCustomer"
- *  "To implement "Epic feature 9", I modified the "class Dart/DartView" by <adding> : <method> : messageCenterMenu, receiveMessage, sendMessage..."
- *  "To implement "Epic feature 9", I modified the "class UserController" by <adding> : <method> : getCurrentUserId, SetCurrentUser, getCurrentUser, getUserWithNameAndPassword..."
- *  "To implement "Epic feature X", I modified the "class X" by <adding | removing> : <attribute | method> : A, B, C..."
- *  "To implement "Epic feature X", I modified the "class X" by <adding | removing> : <attribute | method> : A, B, C..."
- *  "To implement "Epic feature X", I modified the "class X" by <adding | removing> : <attribute | method> : A, B, C..."
+ *  "To implement "Epic feature 8", We modified the "class Dart/DartView" by <adding> : <method> : loginCheckCustomer"
+ *  "To implement "Epic feature 9", We modified the "class Dart/DartView" by <adding> : <method> : messageCenterMenu, receiveMessage, sendMessage..."
+ *  "To implement "Epic feature 9", We modified the "class UserController" by <adding> : <method> : getCurrentUserId, SetCurrentUser, getCurrentUser, getUserWithNameAndPassword..."
+ *  "To implement "Epic feature X", We modified the "class X" by <adding | removing> : <attribute | method> : A, B, C..."
+ *  "To implement "Epic feature X", We modified the "class X" by <adding | removing> : <attribute | method> : A, B, C..."
+ *  "To implement "Epic feature X", We modified the "class X" by <adding | removing> : <attribute | method> : A, B, C..."
  *
  * <Changes from milestone 1 and 2 to milestone 3:
- *  "To implement "Epic feature X", I modified the "class X" by <adding | removing> : <attribute | method> : A, B, C..."
- *  "To implement "Epic feature X", I modified the "class X" by <adding | removing> : <attribute | method> : A, B, C..."
- *  "To implement "Epic feature X", I modified the "class X" by <adding | removing> : <attribute | method> : A, B, C..."
- *  "To implement "Epic feature X", I modified the "class X" by <adding | removing> : <attribute | method> : A, B, C..."
- *  "To implement "Epic feature X", I modified the "class X" by <adding | removing> : <attribute | method> : A, B, C..."
+ *  "To implement "Epic feature X", We modified the "class X" by <adding | removing> : <attribute | method> : A, B, C..."
+ *  "To implement "Epic feature X", We modified the "class X" by <adding | removing> : <attribute | method> : A, B, C..."
+ *  "To implement "Epic feature X", We modified the "class X" by <adding | removing> : <attribute | method> : A, B, C..."
+ *  "To implement "Epic feature X", We modified the "class X" by <adding | removing> : <attribute | method> : A, B, C..."
+ *  "To implement "Epic feature X", We modified the "class X" by <adding | removing> : <attribute | method> : A, B, C..."
  *
  */
 
